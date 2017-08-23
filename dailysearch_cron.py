8a67ff091ef785f3ad6d80fbe24e744a9a383186
Gabriel Bertacco <niteckbj@gmail.com>
2017-08-22 23:40:51 -0300

    Add a workaround to force Daily Searchs
    To enable this option, open your terminal and run
    
        crontab -e
    
    and put the following line at the end of the cron file
    
        */30 * * * * python path_to_bj_sickrage/dailysearch_cron.py API_KEY
    -u SICKRAGE_URL -p SICKRAGE_PORT

---
+++ dailysearch_cron.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import argparse
+import json
+import urllib
+
+def main(url, port, apiKey):
+    backlog = urllib.urlopen(
+        "{}/api/{}/?cmd=backlog".format("http://" + url + ":" + port, apiKey)
+    )
+    jsonBacklog = json.loads(backlog.read())
+    for tvshow in jsonBacklog['data']:
+        indexerid = tvshow['indexerid']
+        episodes = tvshow['episodes']
+        for episode in episodes:
+            season = episodio['season']
+            episodeNumber = episodio['episode']
+            urllib.urlopen(
+                "{}/api/{}/?cmd=episode.search&indexerid={}" + \
+                "&season={}&episode={}".format(
+                    urlSickRage,
+                    apiKey,
+                    indexerid,
+                    season,
+                    episodeNumber,
+                  )
+            )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Execute Sickrage's Daily Search",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        'apiKey',
+        type = str,
+        help = "Sickrage api key"
+    )
+
+    parser.add_argument(
+        '-u',
+        '--url',
+        type = str,
+        default = "localhost",
+        help = "Sickrage Url",
+    )
+
+    parser.add_argument(
+        '-p',
+        '--port',
+        type = str,
+        default = "8081",
+        help="Sickrage port"
+    )
+
+    args = parser.parse_args()
+    main(args.url, args.port, args.apiKey)
